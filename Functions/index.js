ef19901d6bc1317c46b1a6f5756c9bc3b8679af7
Virendra-Verma <vkverma991099@gmail.com>
2024-02-25 19:54:15 +0530

    .

---
+++ Functions/index.js
@@ -0,0 +1,25 @@
+function nice(name) {
+    console.log("Hey"+ name+"you are nice!")
+    console.log("Hey"+ name+"you are good!")
+    console.log("Hey"+ name+"your teserting is good!")
+ 
+    
+}
+nice(" Priya ")
+nice(" Viren ")
+
+
+
+// function sum(a,b){
+//     return a+b
+// }
+// result_1 = sum(2,3)
+// result_2 = sum(4,3)
+// console.log(result)
+
+
+const fun1 = (x)=>{
+    console.log("i am ",x)
+}
+fun1(10);
+fun1(20);
