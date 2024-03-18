0f46bcab29d428542c654e93e15d6ab12e19dcbe
Virendra-Verma <vkverma991099@gmail.com>
2024-03-18 21:59:43 +0530

    .

--- fs and path Modules/main.js
+++ fs and path Modules/main.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 console.log(fs)
 console.log("Starting")
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
+// fs.writeFileSync('hello.txt', 'Hello from Node.js');
+fs.write("hello.txt", "Hello from Node.js, () =>{
+    console.log("File written")
+})
+
 console.log("Ending...")
