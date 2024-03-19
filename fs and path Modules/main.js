388fe5ecf34ff74ac685e5713f86521f896bad07
Virendra-Verma <vkverma991099@gmail.com>
2024-03-19 21:59:57 +0530

    .

--- fs and path Modules/main.js
+++ fs and path Modules/main.js
@@ -1,9 +1,13 @@
+const e = require('express');
 const fs = require('fs');
 console.log(fs)
 console.log("Starting")
 // fs.writeFileSync('hello.txt', 'Hello from Node.js');
-fs.write("hello.txt", "Hello from Node.js, () =>{
+fs.writeFile("hello.txt", "Hello from Node.js", () =>{
     console.log("File written")
+    fs.readFile("hello.txt", "utf8", (err, data) =>{
+     console.log(err, data)
+})
 })
 
 console.log("Ending...")
